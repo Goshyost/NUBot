174bc08175b891ccabf8d82fb477738b55c72642
Goshyost <goshyostdeveloper@gmail.com>
2024-09-25 01:32:18 -0700

    Changed Avatar command to Profile command
    I changed it because, profile command is cool, and avatar is boring, hasta aqui mi reporte joaquin

--- slashCommands/avatar.js
+++ slashCommands/avatar.js
@@ -1,14 +1,19 @@
 const { SlashCommandBuilder } = require("discord.js");
 const { PermissionFlagsBits } = require("discord.js")
 const Discord = require("discord.js")
+const db = require("megadb")
+const warns = new db.crearDB("warns")
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("avatar")
-    .setDescription("Ve el avatar de otro usuario del servidor")
+    .setName("perfil")
+    .setDescription("Muestra informacion sobre un usuario")
     .addUserOption(p => p.setName("usuario")
-                  .setDescription("Especica el usuario, deja esto en blanco para ver tu foto de perfil")
-                  .setRequired(true)),
+                  .setDescription("Especica el usuario de el que quieres ver su info")
+                  .setRequired(true))
+    .addBooleanOption(p => p.setName("oculto")
+                     .setDescription("Si seleccionas True, el mensaje solamente lo veras tu")
+                     .setRequired(false)),
     
   /**
    *
@@ -18,15 +23,11 @@ module.exports = {
 
   async run(client, interaction) {
   
+  if(interaction.user.id !== "1144403268076830730") return interaction.reply({ content: "This command is currently under rework...", ephemeral: true })
+   
   let usuario = interaction.options.getUser("usuario") 
   let miembro = interaction.options.getMember("usuario")
   
-  const AvatarEmbed = new Discord.EmbedBuilder()
-  .setTitle("Avatar de "+usuario.displayName)
-  .setColor("Yellow")
-  .setImage(usuario.displayAvatarURL({ size: 1024, dynamic: true }))
-  .setFooter({ text: "NEW UNDERGROUNDS" })
-  .setTimestamp()
   
   const AvatarEmbed2 = new Discord.EmbedBuilder()
   .setTitle(`Avatar de ${interaction.user.username}`)
@@ -35,28 +36,71 @@ module.exports = {
   .setFooter({ text: "NEW UNDERGROUNDS" })
   .setTimestamp()
   
+  let especial;
+      
+        if (miembro.roles.cache.some(role => role.name === 'Usuario')) {
+          especial = "Ningun rol Especial..."
+        }
         if (miembro.roles.cache.some(role => role.name === 'Cadete')) {
-          AvatarEmbed.setDescription("**Miembro de la Academia de Moderadores de NEW UNDERGROUNDS 🛡️**")
+          especial = "Miembro de la Academia de Moderadores 🛡️"
         }
         if (miembro.roles.cache.some(role => role.name === 'Sub-Teniente')) {
-          AvatarEmbed.setDescription("**Miembro de la Academia de Moderadores de NEW UNDERGROUNDS 🛡️**")
+          especial = "Miembro de la Academia de Moderadores 🛡️"
         }
         if (miembro.roles.cache.some(role => role.name === 'Teniente')) {
-          AvatarEmbed.setDescription("**Miembro de la Academia de Moderadores de NEW UNDERGROUNDS 🛡️**")
+          especial = "Miembro de la Academia de Moderadores 🛡️"
         }
         if (miembro.roles.cache.some(role => role.name === 'Mayor')) {
-          AvatarEmbed.setDescription("**Miembro de la Academia de Moderadores de NEW UNDERGROUNDS 🛡️**")
+          especial = "Miembro de la Academia de Moderadores 🛡️"
         }
         if (miembro.roles.cache.some(role => role.name === 'Coronel')) {
-          AvatarEmbed.setDescription("**Miembro de la Academia de Moderadores de NEW UNDERGROUNDS 🛡️**")
+          especial = "Miembro de la Academia de Moderadores 🛡️"
         }
         if (miembro.roles.cache.some(role => role.name === 'General')) {
-          AvatarEmbed.setDescription("**Miembro de la Academia de Moderadores de NEW UNDERGROUNDS 🛡️**")
+          especial = "Miembro de la Academia de Moderadores 🛡️"
         }
+        if (miembro.roles.cache.some(role => role.name === 'Streamer')) {
+          especial = "Creador de Contenido 🟣"
+        }
+        if (miembro.roles.cache.some(role => role.name === 'Youtuber')) {
+          especial = "Creador de Contenido 🔴"
+        }
+        if (miembro.roles.cache.some(role => role.name === 'DONADOR UwU')) {
+          especial = "Donador 💸"
+        }
+        if (miembro.roles.cache.some(role => role.name === 'Artista')) {
+          especial = "Artista 🎨"
+        }
+        if (miembro.roles.cache.some(role => role.name === 'Presidente de las instalaciones')) {
+          especial = "Nivel 50 🏆"
+        }
+      let booster;
+      
+      if(miembro.premiumSince === null){
+          booster = "`No`"
+      } else {
+          booster = `<t:${Math.round(usuario.premiumSinceTimestamp / 1000)}:R>`
+      }
+      
+      let w;
+      if(warns.tiene(usuario.id) === true){
+          w = await warns.obtener(usuario.id)
+      } else {
+          w = "`No tiene Advertencias`"
+      }
+  const AvatarEmbed = new Discord.EmbedBuilder()
+  .setTitle("Perfil de "+usuario.displayName)
+  .setDescription(`🎂 Creado: **<t:${Math.round(usuario.createdTimestamp / 1000)}:R>**\n 🛬 Se unio: **<t:${Math.round(miembro.joinedTimestamp / 1000)}:R>**\n⭐ Especial: **\`${especial}\`**\n💎 Booster: **${booster}**\n🪪 ID: **\`${usuario.id}\`**\n👤 Rol mas Alto: <@&${miembro.roles.highest.id}>\n⚠️ Warns: **${w}**`)
+  .setColor(miembro.displayHexColor)
+  .setThumbnail(usuario.displayAvatarURL())
+  .setFooter({ text: "NEW UNDERGROUNDS" })
+  .setTimestamp()
   
+  
+  let oculto = interaction.options.getBoolean("oculto") ?? false
   if(usuario){
       
-      interaction.reply({ embeds: [AvatarEmbed] })
+      interaction.reply({ embeds: [AvatarEmbed], ephemeral: oculto })
       
   }
       
